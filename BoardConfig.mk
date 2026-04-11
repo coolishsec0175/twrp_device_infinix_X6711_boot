bb9f7e1d6a5bdbac330203dbbf4332de6f7b2ec6
XTENSEI <andriegalutera@gmail.com>
2026-04-11 12:15:05 +0800

    X6711: Fix path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6711
+DEVICE_PATH := device/infinix/X6711
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
