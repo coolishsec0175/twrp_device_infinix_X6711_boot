787c397a46e2ab41725246bad1fac0ae7869577c
XTENSEI <andriegalutera@gmail.com>
2026-04-11 12:16:45 +0800

    X6711: omni > twrp

--- twrp_X6711.mk
+++ twrp_X6711.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6711 device
-$(call inherit-product, device/infinix/Infinix-X6711/device.mk)
+$(call inherit-product, device/infinix/X6711/device.mk)
 
-PRODUCT_DEVICE := Infinix-X6711
-PRODUCT_NAME := omni_Infinix-X6711
+PRODUCT_DEVICE := X6711
+PRODUCT_NAME := twrp_X6711
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6711
 PRODUCT_MANUFACTURER := infinix
