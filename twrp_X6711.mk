865e554c5615794e81b746118c8e135ecb05e940
charles_dxd <17maydayparade@gmail.com>
2026-07-29 15:58:37 +0800

    Update twrp_X6711.mk

--- twrp_X6711.mk
+++ twrp_X6711.mk
@@ -8,6 +8,13 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+#Inherit emulated_storage properties
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit userspace reboot properties
+$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -22,8 +29,3 @@ PRODUCT_MODEL := Infinix X6711
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_x6711_h333-user 12 SP1A.210812.016 753368 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6711-EU/Infinix-X6711:12/SP1A.210812.016/241121V2535:user/release-keys
