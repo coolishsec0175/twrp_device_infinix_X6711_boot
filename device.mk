660c55135514ca7d8f43e7a3ba2a43b528497910
padoru69869 <padoru69869@users.noreply.github.com>
2026-08-17 20:17:22 +0800

    Fix bootloop: LZ4 ramdisk, boot@1.0 HAL, crypto/init scripts, fstab
    - BoardConfig.mk: switch from LZMA to LZ4 ramdisk compression
    - device.mk: change boot HAL from 1.2-mtkimpl to 1.0-impl, add update_engine packages
    - init.recovery.mt6833.rc: add crypto.ready handling, UFS boot support, vold early start, mount_all, vendor-prefixed services, OrangeFox theme directories
    - recovery.fstab: add first_stage_mount, AVB flags, improved /data mount options
    - twrp.flags: add missing MediaTek partitions (lk, proinfo, tkv, gz, mcupm, sspm, ccu, scp, dpm, pi_img, md1img, apusys)
    - system.prop: fix crypto properties, add gatekeeper and TEE settings

--- device.mk
+++ device.mk
@@ -7,12 +7,27 @@
 
 LOCAL_PATH := device/infinix/X6711
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery \
-    bootctrl.mt6833 \
-    bootctrl.mt6833.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6833
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
 
 # Keymaster
 PRODUCT_PACKAGES += \
